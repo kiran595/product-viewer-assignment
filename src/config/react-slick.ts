92d98a0c663ae99db8bd7a343975ce58c72c9f91
kiran595 <maharjank595@gmail.com>
2025-03-28 10:37:50 +0545

    slider speed added to react slick

--- src/config/react-slick.ts
+++ src/config/react-slick.ts
@@ -2,7 +2,9 @@ export const multiImageConfig = {
   dots: false,
   infinite: true,
   arrows: true,
-  speed: 500,
+  speed: 1000,
+  autoplaySpeed: 1500,
+  autoplay: true,
   slidesToShow: 3,
   cssEase: 'linear'
 };
